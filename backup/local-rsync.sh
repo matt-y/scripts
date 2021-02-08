77cf13310bd6e5cd45ac8cc1fdc2fe0b5212c739
Matthew Young <radicalmatt@fastmail.com>
2021-02-08 15:49:05 -0600

    Fixed backup script error recoevery
    The existing solution was half finished and not quite functional.
    
    - moves rsync from a subshell command to a background process script
    - traps signals, and then waits for the background task to complete
    - background task is now appropriately killed when a signal is received
    - fixed up some echo output, cleaned up a bit

--- backup/local-rsync.sh
+++ backup/local-rsync.sh
@@ -18,20 +18,36 @@
 NC='\033[0m'
 RED='\033[0;31m'
 
+
+BACKUP_SOURCE=$1
+BACKUP_TARGET=$2
+readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
+readonly BACKUP_DIR="${BACKUP_TARGET}/backups"
+readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
+readonly LATEST_LINK="${BACKUP_DIR}/latest"
+
 usage() {
     echo -e "\nUsage: ${0##*/} BACKUP_SOURCE BACKUP_TARGET\n"
 }
 
 cleanup_rsync() {
-    echo "${RED}Bad backup to $1, moving to BAD_BACKUP${NC}"
-    mv "$1" BAD_BACKUP
+    BAD_BACKUP_PATH="$BACKUP_PATH"
+    BAD_BACKUP_DESTINATION="$BACKUP_DIR/BAD_BACKUP"
+    echo -e "${RED}Cleaning up${NC}"
+    echo -e "${RED}Bad backup to $BAD_BACKUP_PATH, moving to $BAD_BACKUP_DESTINATION${NC}"
 
-    rm -rI BAD_BACKUP
+    mv "$BAD_BACKUP_PATH" "$BAD_BACKUP_DESTINATION"
 }
 
 handle_signals() {
-    echo "${RED}Recieved signal, stopping and cleaning${NC}"
-    cleanup_rsync "$1"
+    echo -e "${RED}Recieved signal, stopping and cleaning${NC}"
+
+    # Stop provided background rsync pid
+    kill -2 "$1"
+    # perform cleanup 
+    cleanup_rsync
+
+    exit 1
 }
 
 if [ $# -ne 2 ]
@@ -47,11 +63,8 @@ then
     exit 0
 fi
 
-echo "beginning local rsync backup"
+echo -e "beginning local rsync backup"
 echo -e "\n"
-
-BACKUP_SOURCE=$1
-BACKUP_TARGET=$2
 
 if [ ! -d "$BACKUP_SOURCE" ]
 then
@@ -70,29 +83,32 @@ else
     fi
 fi
 
-readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
-readonly BACKUP_DIR="${BACKUP_TARGET}/backups"
-readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
-readonly LATEST_LINK="${BACKUP_DIR}/latest"
-
 mkdir -p "$BACKUP_DIR"
 
-echo "rsync source: ${BACKUP_SOURCE}"
-echo "rsync target: ${BACKUP_PATH}"
+echo -e "rsync source: ${BACKUP_SOURCE}"
+echo -e "rsync target: ${BACKUP_PATH}"
 echo -e "\n"
 
-trap 'handle_signals "$BACKUP_PATH"' SIGINT SIGTERM
-
-if rsync_status=$(rsync -avq --delete \
+# Start rsync process in the background 
+rsync -avq --delete --progress \
     "${BACKUP_SOURCE}/" \
     --link-dest "${LATEST_LINK}" \
     --exclude=".cache" \
-    "${BACKUP_PATH}");
-then
-    echo "updating latest link"
+    "${BACKUP_PATH}" &
+
+# Save pid of rsync
+rsync_pid=$!
+
+# Set a trap for this potentially long running process 
+trap 'handle_signals "$rsync_pid"' SIGINT SIGTERM
+
+# Wait for background jobs
+if wait; then
+    echo -e "Update completed"
+    echo -e "updating latest link"
     rm -rf "${LATEST_LINK}"
     ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
 else
-    echo "${RED}Error: rsync failure, cleaning up $BACKUP_PATH"
-    cleanup_rsync "$BACKUP_PATH"
+    echo -e "${RED}Error: rsync failure, cleaning up $BACKUP_PATH"
+    cleanup_rsync
 fi
